5868b2251720a6216503ce6f0f8ec736730d41b5
Snehashish Saha <saha.snehashish@gmail.com>
2024-05-16 11:46:58 +0530

    Purchase order migration

---
+++ RMS.DataAccess/Migrations/20240515090336_AddOrderHeaderAndItemToDb.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RMS.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOrderHeaderAndItemToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PurchaseOrderHeaders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ApplicationUserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Store = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RequestNo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ReferenceNo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ReferenceDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    Budget = table.Column<double>(type: "float", nullable: false),
+                    ExpectedDelivery = table.Column<DateOnly>(type: "date", nullable: false),
+                    OrderStatus = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PurchaseOrderHeaders", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PurchaseOrderHeaders_AspNetUsers_ApplicationUserId",
+                        column: x => x.ApplicationUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PurchaseOrderItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OrderHeaderId = table.Column<int>(type: "int", nullable: false),
+                    ProductId = table.Column<int>(type: "int", nullable: false),
+                    Count = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PurchaseOrderItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PurchaseOrderItems_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PurchaseOrderItems_PurchaseOrderHeaders_OrderHeaderId",
+                        column: x => x.OrderHeaderId,
+                        principalTable: "PurchaseOrderHeaders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PurchaseOrderHeaders_ApplicationUserId",
+                table: "PurchaseOrderHeaders",
+                column: "ApplicationUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PurchaseOrderItems_OrderHeaderId",
+                table: "PurchaseOrderItems",
+                column: "OrderHeaderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PurchaseOrderItems_ProductId",
+                table: "PurchaseOrderItems",
+                column: "ProductId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PurchaseOrderItems");
+
+            migrationBuilder.DropTable(
+                name: "PurchaseOrderHeaders");
+        }
+    }
+}
